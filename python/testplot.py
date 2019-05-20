0691fddce8a6ee2ba4f79a1aef83fc4c4955184e
Chris North <chris.north@astro.cf.ac.uk>
2019-05-20 13:06:09 +0100

    tweaked test plot

--- python/testplot.py
+++ python/testplot.py
@@ -3,8 +3,8 @@ import healpy as hp
 import gwcat
 
 plot.ion()
-map=hp.read_map('data/fits/S190519bj_bayestar.fits.gz')
-# gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0.5,zoomlim=None,rotmap=False,verbose=True,cbg='w')
-# gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0.5,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180)
-gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0.5,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180,grid=True)
-# gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0.5,zoomlim=None,rotmap=True,verbose=True,cbg='w')
+# map=hp.read_map('data/fits/S190519bj_bayestar.fits.gz')
+map=gwcat.plotloc.makePlot(ev='S190519bj',proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=False,verbose=True,cbg='w')
+gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180)
+gwcat.plotloc.makePlot(ev='S190519bj',proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180,grid=True)
+gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=True,verbose=True,cbg='w')
