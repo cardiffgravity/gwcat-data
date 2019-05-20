eb24ca021ca568423262ced2d83c903e5c776daa
Chris North <chris.north@astro.cf.ac.uk>
2019-05-20 12:51:24 +0100

    added code to plot grid

---
+++ python/testplot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plot
+import healpy as hp
+import gwcat
+
+plot.ion()
+map=hp.read_map('data/fits/S190519bj_bayestar.fits.gz')
+# gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0.5,zoomlim=None,rotmap=False,verbose=True,cbg='w')
+# gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0.5,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180)
+gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0.5,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180,grid=True)
+# gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0.5,zoomlim=None,rotmap=True,verbose=True,cbg='w')
