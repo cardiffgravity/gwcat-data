137b3ce941847047e86ad6d0c41e0304fb33490e
Chris North <chris.north@astro.cf.ac.uk>
2019-05-22 17:54:39 +0100

    uncommented gwosc data

--- updatecat.py
+++ updatecat.py
@@ -24,9 +24,8 @@ baseurl=args.baseurl
 
 if update==True:
     gc=gwcat.GWCat(fileIn=os.path.join(dataDir,'gwosc_gracedb.json'),dataDir=dataDir,baseurl=baseurl,verbose=verbose)
-    # gdb=json.load(open(os.path.join(dataDir,'gracedb.json')))
-    # gwoscdata=json.load(open(os.path.join(dataDir,'gwosc.json')))
-    # print(knownEvents)
+    gdb=json.load(open(os.path.join(dataDir,'gracedb.json')))
+    gwoscdata=json.load(open(os.path.join(dataDir,'gwosc.json')))
     knownEvents=gc.getTimestamps()
 
     gwoscdata=gwcat.gwosc.getGwosc(export=True,dirOut=dataDir,verbose=verbose)
