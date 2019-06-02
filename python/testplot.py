c182e75e94d90d01a02aa6abcba94d1f25485e0e
Chris North <chris.north@astro.cf.ac.uk>
2019-06-02 06:35:06 +0100

    added logos to images

--- python/testplot.py
+++ python/testplot.py
@@ -4,7 +4,7 @@ import gwcat
 
 plot.ion()
 # map=hp.read_map('data/fits/S190519bj_bayestar.fits.gz')
-map=gwcat.plotloc.makePlot(ev='S190519bj',proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=False,verbose=True,cbg='w')
-gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180)
-gwcat.plotloc.makePlot(ev='S190519bj',proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180,grid=True)
-gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=True,verbose=True,cbg='w')
+map=gwcat.plotloc.makePlot(ev='S190519bj',proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=False,verbose=True,cbg='w',dirData='../data/fits/',addCredit=True,addLogos=True)
+gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='cart',plotcont=False,smooth=0,zoomlim=None,rotmap=False,verbose=True,cbg='w',RAcen=180,dirData='../data/fits/',addCredit=True,addLogos=True)
+gwcat.plotloc.makePlot(ev='S190519bj',proj='cart',plotcont=False,smooth=0,zoomlim=0.8,rotmap=True,verbose=True,cbg='w',grid=False,dirData='../data/fits/',addCredit=True,addLogos=True)
+# gwcat.plotloc.makePlot(ev='S190519bj',mapIn=map,proj='moll',plotcont=False,smooth=0,zoomlim=None,rotmap=True,verbose=True,cbg='w',dirData='../data/fits/')
