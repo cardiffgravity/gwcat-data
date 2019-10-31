da18ceabb51c78683517a95e549a1833f6ead866
Chris North <chris.north@astro.cf.ac.uk>
2019-10-31 08:22:20 +0000

    Add write-to-csv option

--- updatecat.py
+++ updatecat.py
@@ -62,3 +62,6 @@ json.dump(gcdat,open(os.path.join(dataDir,'gwosc_gracedb.min.json'),'w'))
 gwcat.json2jsonp(os.path.join(dataDir,'gwosc_gracedb.json'),os.path.join(dataDir,'gwosc_gracedb.jsonp'))
 gwcat.json2jsonp(os.path.join(dataDir,'gwosc_gracedb.min.json'),os.path.join(dataDir,'gwosc_gracedb.min.jsonp'))
 
+#export data to CSV files
+gc.exportCSV(os.path.join(dataDir,'gwosc_gracedb.csv'),verbose=True,dictfileout=os.path.join(dataDir,'parameters.csv'))
+
