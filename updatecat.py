818f60731134f7c1c3ca5327bc41596eb8cbf669
Chris North <chris.north@astro.cf.ac.uk>
2020-11-05 21:38:14 +0000

    update from gwcat-data-dev

--- updatecat.py
+++ updatecat.py
@@ -5,32 +5,37 @@ statF.write('pending\n')
 statF.close()
 
 import sys,os
+sys.path.insert(0,os.path.join(os.getcwd(),'python'))
+
+import gwcatpy
+import json
 import argparse
 
 parser=argparse.ArgumentParser(prog="updatecat.py", description="Updates the gwcat-data database")
-parser.add_argument('--codedir', dest='codedir', type=str, default='python', help='directory in which dev code is stored')
 parser.add_argument('-u','--update', dest='update', action='store_true', default=False, help='Update from GWOSC and GraceDB source')
 parser.add_argument('-v','--verbose', dest='verbose', action='store_true', default=False, help='Set to print more helpful text to the screen')
 parser.add_argument('-o','--overwrite', dest='overwrite', action='store_true', default=False, help='Regenerate and overwrite image files')
 parser.add_argument('-f','--forceupdate', dest='forceupdate', action='store_true', default=False, help='Force (re)download of files')
 parser.add_argument('-s','--skymaps', dest='skymaps', action='store_true', default=False, help='Plot skymaps')
 parser.add_argument('-m','--forcemap', dest='forcemap', action='store_true', default=False, help='Force (re)download of fits files')
+parser.add_argument('-5','--forceh5', dest='forceh5', action='store_true', default=False, help='Force (re)extraction of HDF files')
 parser.add_argument('-g','--gravoscope', dest='gravoscope', action='store_true', default=False, help='Update Gravoscope tiles')
 parser.add_argument('-w','--waveforms', dest='waveforms', action='store_true', default=False, help='Update Waveforms')
 parser.add_argument('--manual', dest='manual', action='store_true', default=False, help='Read in manual data')
 parser.add_argument('-d','--datadir', dest='datadir', type=str, default='data/', help='directory in which data is stored')
 parser.add_argument('-l','--datelim', dest='datelim', type=float, default=999, help='number of days to go back in time')
 parser.add_argument('-b','--baseurl', dest='baseurl', type=str, default='https://data.cardiffgravity.org/gwcat-data/', help='Base URL to prepend to relative links [Default=https://data.cardiffgravity.org/gwcat-data/]')
-parser.add_argument('-t','--tilesurl', dest='tilesurl', type=str, default='https://gravity.astro.cf.ac.uk/gwcat-data/', help='Base URL to prepend to relative links for tiles [Default=https://ligo.gravity.cf.ac.uk/chris.north/gwcat-data/]')
+parser.add_argument('-t','--tilesurl', dest='tilesurl', type=str, default='Default=https://ligo.gravity.cf.ac.uk/chris.north/gwcat-data/', help='Base URL to prepend to relative links for tiles [Default=Default=https://ligo.gravity.cf.ac.uk/chris.north/gwcat-data/]')
 parser.add_argument('--log',dest='logfile',type=str, default='logs/gdb_updates.log', help='File to output GraceDB logs to. [Default=logs/gdb_updates.log]')
 parser.add_argument('--skipgracedb',dest='skipgracedb',action='store_true', default=False, help='Set to skip GraceDB load')
+parser.add_argument('--devMode',dest='devMode',action='store_true', default=False, help='Set to use dev mode (requires LVK login)')
 args=parser.parse_args()
 dataDir=args.datadir
-codedir=args.codedir
 update=args.update
 verbose=args.verbose
 forceupdate=args.forceupdate
 forcemap=args.forcemap
+forceh5=args.forceh5
 overwrite=args.overwrite
 baseurl=args.baseurl
 tilesurl=args.tilesurl
@@ -38,58 +43,62 @@ gravoscope=args.gravoscope
 waveforms=args.waveforms
 datelim=args.datelim
 logfile=args.logfile
-skymaps=args.skymaps
 skipgracedb=args.skipgracedb
+skymaps=args.skymaps
+devMode=args.devMode
 
-if os.getcwd().split('/')[-1]=='python':
-    relDir='../'
+if devMode:
+    mode='dev'
 else:
-    relDir='./'
-sys.path.insert(0,os.path.join(relDir,codedir))
+    mode=None
 
-import gwcatpy
-import json
+fileIn=os.path.join(dataDir,'gwosc_gracedb.json')
+
+print('\n\n*****\nImporting from local file\n*****\n\n')
+gc=gwcatpy.GWCat(fileIn=fileIn,dataDir=dataDir,mode=mode,baseurl=baseurl)
 
 if update==True:
-    gc=gwcatpy.GWCat(fileIn=os.path.join(dataDir,'gwosc_gracedb.json'),
-        dataDir=dataDir,baseurl=baseurl,verbose=verbose)
-    gdb=json.load(open(os.path.join(dataDir,'gracedb.json')))
-    gwoscdata=json.load(open(os.path.join(dataDir,'gwtc1.json')))
+
+    print('\n\n*****\nReading GWTC...\n*****\n\n')
+    gwtcdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode)
+    print('\n\n*****\nImporting GWTC...\n*****\n\n')
+    gc.importGWTC(gwtcdata,verbose=verbose, devMode=devMode)
     knownEvents=gc.getTimestamps()
-
-    mandata=gwcatpy.getManual(export=True,dirOut=dataDir,verbose=verbose)
+    json.dump(gwtcdata,open(os.path.join(dataDir,'gwtc.min.json'),'w'))
     if not skipgracedb:
+        print('\n\n*****\nReading GraceDB...\n*****\n\n')
         gdb=gwcatpy.gracedb.getSuperevents(export=True,dirOut=dataDir,verbose=verbose,
-            knownEvents=knownEvents,forceUpdate=forceupdate,datelim=datelim,logFile=logfile)
-    gwtc1data=gwcatpy.gwosc.getGWTC1(export=True,dirOut=dataDir,verbose=verbose)
-    json.dump(gwtc1data,open(os.path.join(dataDir,'gwtc1.min.json'),'w'))
-    json.dump(gdb,open(os.path.join(dataDir,'gracedb.min.json'),'w'))
+        knownEvents=knownEvents,forceUpdate=forceupdate,datelim=datelim,logFile=logfile)
+        json.dump(gdb,open(os.path.join(dataDir,'gracedb.min.json'),'w'))
+                    
+        print('\n\n*****\nimporting GraceDB...\n*****\n\n')
+        gc.importGraceDB(gdb,verbose=verbose,forceUpdate=forceupdate)
+        print('\n\n*****\nmatching GraceDB entries...\n*****\n\n')
+        gc.matchGraceDB(verbose=verbose)
+        print('\n\n*****\nremoving unnecessary GraceDB candidates\n*****\n\n')
+        gc.removeCandidates(verbose=verbose)
+    
+    print('\n\n*****\nAdding manual references...\n*****\n\n')    
+    gc.addRefs(verbose=verbose)
 
-    print('importing Manual Data...')
-    gc.importManual(mandata,verbose=verbose)
-    print('importing GWTC1...')
-    gc.importGWTC1(gwtc1data,verbose=verbose)
-    print('importing GraceDB...')
-    gc.importGraceDB(gdb,verbose=verbose,forceUpdate=forceupdate)
-    print('updating data from H5 files...')
-    gc.updateH5(verbose=verbose,forceUpdate=False)
-    print('setting precision...')
+    print('\n\n*****\nUpdating data from H5\n*****\n\n')
+    gc.updateH5(verbose=verbose,forceUpdate=forceupdate,forceUpdateData=forceh5)
+
+    print('\n\n*****\nsetting precision...\n*****\n\n')
     gc.setPrecision(extraprec=1,verbose=verbose)
-    print('matching GraceDB entries')
-    gc.matchGraceDB(verbose=verbose)
-    print('removing unnecessary GraceDB candidates')
-    gc.removeCandidates(verbose=verbose)
-    print('adding references')
-    gc.addrefs(verbose=verbose)
+
+    print('\n\n*****\nUpdating maps\n*****\n\n')
+    gc.updateMaps(verbose=verbose,forceUpdate=forcemap)
+    
 
 else:
     print('importing from local file')
-    gc=gwcatpy.GWCat(fileIn=os.path.join(dataDir,'gwosc_gracedb.json'),dataDir=dataDir)
+    gc=gwcatpy.GWCat(fileIn=os.path.join(dataDir,'gwosc_gracedb.json'),dataDir=dataDir,mode=mode)
 
-gc.updateMaps(verbose=verbose,forceUpdate=forcemap)
+    
 logfileMaps=logfile+'_maps'
 if skymaps:
-    print('Plotting Skymaps')
+    print('\n\n*****\nPlotting maps\n*****\n\n')
     gc.plotMapPngs(verbose=verbose,overwrite=overwrite,logFile=logfileMaps)
 else:
     if os.path.exists(logfileMaps):
@@ -99,13 +108,12 @@ else:
         fM.close()
 
 if gravoscope:
-    print('Updating gravoscope')
-    gc.makeGravoscopeTiles(verbose=True,maxres=6,tilesurl=tilesurl)
+    print('\n\n*****\nUpdating gravoscope\n*****\n\n')
+    gc.makeGravoscopeTiles(verbose=verbose,maxres=6,tilesurl=tilesurl)
 
 if waveforms:
-    print('Updating waveforms')
-    gc.makeWaveforms(verbose=True,overwrite=False)
-
+    print('\n\n*****\nUpdating waveforms\n*****\n\n')
+    gc.makeWaveforms(verbose=verbose,overwrite=False)
 
 # export library
 gc.exportJson(os.path.join(dataDir,'gwosc_gracedb.json'))
