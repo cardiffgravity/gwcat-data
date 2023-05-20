81830a7a1347437ad65b5607a5fb4fe119adcc66
Chris North <chris.north@astro.cf.ac.uk>
2023-05-20 01:29:28 +0100

    updated (minor bug fix)

--- updatecat.py
+++ updatecat.py
@@ -30,6 +30,7 @@ parser.add_argument('-t','--tilesurl', dest='tilesurl', type=str, default='https
 parser.add_argument('--log',dest='logfile',type=str, default='logs/gdb_updates.log', help='File to output GraceDB logs to. [Default=logs/gdb_updates.log]')
 parser.add_argument('--gracedb',dest='gracedb',action='store_true', default=False, help='Set to include GraceDB load')
 parser.add_argument('--skipgwosc',dest='skipgwosc',action='store_true', default=False, help='Set to skip GWOSC load')
+parser.add_argument('--skipmarginal',dest='skipmarginal',action='store_true', default=False, help='Set to skip marginal catalogue load')
 parser.add_argument('--devMode',dest='devMode',action='store_true', default=False, help='Set to use dev mode (requires LVK login)')
 parser.add_argument('--skiph5',dest='skiph5',action='store_true', default=False, help='Set to skip using H5 files')
 parser.add_argument('--blank',dest='blank',action='store_true', default=False, help='Set to start from blank file')
@@ -49,6 +50,7 @@ datelim=args.datelim
 logfile=args.logfile
 ImportGracedb=args.gracedb
 skipGwosc=args.skipgwosc
+skipMarginal=args.skipmarginal
 skiph5=args.skiph5
 skymaps=args.skymaps
 devMode=args.devMode
@@ -77,20 +79,21 @@ if update==True:
         print('\n\n*****\nImporting GWTC...\n*****\n\n')
         gc.importGWTC(gwtcdata,verbose=verbose, devMode=devMode,catalog='GWTC',forceOverwrite=forceupdate)
 
-        print('\n\n*****\nReading GWTC-3-marginal...\n*****\n\n')
-        gwtc3margdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode,catalog='GWTC-3-marginal',sess=sess)
-        print('\n\n*****\nImporting GWTC-3-marginal...\n*****\n\n')
-        gc.importGWTC(gwtc3margdata,verbose=verbose, devMode=devMode,catalog='GWTC-3-marginal',forceOverwrite=True)
+        if not skipMarginal:
+            print('\n\n*****\nReading GWTC-3-marginal...\n*****\n\n')
+            gwtc3margdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode,catalog='GWTC-3-marginal',sess=sess)
+            print('\n\n*****\nImporting GWTC-3-marginal...\n*****\n\n')
+            gc.importGWTC(gwtc3margdata,verbose=verbose, devMode=devMode,catalog='GWTC-3-marginal',forceOverwrite=True)
 
-        print('\n\n*****\nReading GWTC-2.1-marginal...\n*****\n\n')
-        gwtc21margdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode,catalog='GWTC-2.1-marginal',sess=sess)
-        print('\n\n*****\nImporting GWTC-2.1-marginal...\n*****\n\n')
-        gc.importGWTC(gwtc21margdata,verbose=verbose, devMode=devMode,catalog='GWTC-2.1-marginal',forceOverwrite=True)
+            print('\n\n*****\nReading GWTC-2.1-marginal...\n*****\n\n')
+            gwtc21margdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode,catalog='GWTC-2.1-marginal',sess=sess)
+            print('\n\n*****\nImporting GWTC-2.1-marginal...\n*****\n\n')
+            gc.importGWTC(gwtc21margdata,verbose=verbose, devMode=devMode,catalog='GWTC-2.1-marginal',forceOverwrite=True)
 
-        print('\n\n*****\nReading GWTC-1-marginal...\n*****\n\n')
-        gwtc1margdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode,catalog='GWTC-1-marginal',sess=sess)
-        print('\n\n*****\nImporting GWTC-1-marginal...\n*****\n\n')
-        gc.importGWTC(gwtc1margdata,verbose=verbose, devMode=devMode,catalog='GWTC-1-marginal',forceOverwrite=True)
+            print('\n\n*****\nReading GWTC-1-marginal...\n*****\n\n')
+            gwtc1margdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode,catalog='GWTC-1-marginal',sess=sess)
+            print('\n\n*****\nImporting GWTC-1-marginal...\n*****\n\n')
+            gc.importGWTC(gwtc1margdata,verbose=verbose, devMode=devMode,catalog='GWTC-1-marginal',forceOverwrite=True)
 
         json.dump(gwtcdata,open(os.path.join(dataDir,'gwtc.min.json'),'w'))
     
