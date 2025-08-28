3d3c254e7cfb1dc46160e1754c73a5f66528f1f6
Chris North <chris.north@astro.cf.ac.uk>
2025-08-28 15:49:30 +0100

    remove import of O4 Discovery Papers

--- updatecat.py
+++ updatecat.py
@@ -92,10 +92,10 @@ if update==True:
         print('\n\n*****\nImporting GWTC...\n*****\n\n')
         gc.importGWTC(gwtcdata,verbose=verbose, devMode=devMode,catalog='GWTC',forceOverwrite=forceupdate)
 
-        print('\n\n*****\nReading O4a Discovery Papers...\n*****\n\n')
-        gwtcdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode,catalog='O4_Discovery_Papers',sess=sess)
-        print('\n\n*****\nImporting O4a Discovery Papers...\n*****\n\n')
-        gc.importGWTC(gwtcdata,verbose=verbose, devMode=devMode,catalog='O4_Discovery_Papers',forceOverwrite=True)
+        # print('\n\n*****\nReading O4a Discovery Papers...\n*****\n\n')
+        # gwtcdata=gwcatpy.gwosc.getGWTC(export=True,dirOut=dataDir,verbose=verbose,devMode=devMode,catalog='O4_Discovery_Papers',sess=sess)
+        # print('\n\n*****\nImporting O4a Discovery Papers...\n*****\n\n')
+        # gc.importGWTC(gwtcdata,verbose=verbose, devMode=devMode,catalog='O4_Discovery_Papers',forceOverwrite=True)
 
         if not skipMarginal:
             print('\n\n*****\nReading GWTC-3-marginal...\n*****\n\n')
